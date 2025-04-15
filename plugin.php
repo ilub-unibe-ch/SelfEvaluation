c4f61c1704ab629f551259a9beed18144b928362
Fabian Schmid <fabian@sr.solutions>
2025-04-15 22:33:05 +0200

    [FIX] supported versions

--- plugin.php
+++ plugin.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 $id = 'xsev';
-$version = '3.3.0';
-$ilias_min_version = '9.0.0';
-$ilias_max_version = '9.999.999';
+$version = '4.0.1';
+$ilias_min_version = '9.0';
+$ilias_max_version = '9.999';
 $supports_export = true;
 
 $responsible = "Timon Amstutz - University of Bern";
